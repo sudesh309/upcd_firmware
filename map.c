9ce86825062cdd5b7c56f0fe3f7f4b8f278a9976
Anmol Sarma <me@anmolsarma.in>
2014-01-26 19:24:09 +0530

    Added MapPin and MapPort methods.

---
+++ map.c
@@ -0,0 +1,51 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_memmap.h"
+#include "peripheral_desc.h"
+#include "driverlib/gpio.h"
+
+uint32_t MapPin(ioPin_t ioPin)
+{
+    switch(ioPin.pin) {
+        case 0:
+            return GPIO_PIN_0;
+        case 1: 
+            return GPIO_PIN_1;
+        case 2:
+            return GPIO_PIN_2;
+        case 3: 
+            return GPIO_PIN_3;
+        case 4:
+            return GPIO_PIN_4;
+        case 5: 
+            return GPIO_PIN_5;
+        case 6:
+            return GPIO_PIN_6;
+        case 7: 
+            return GPIO_PIN_7;
+        default:
+            return 0x00000000;
+    }
+}
+
+uint32_t MapPort(ioPin_t ioPin)
+{
+    switch(ioPin.port) {
+        case 1:
+            return GPIO_PORTA_BASE;
+        case 2: 
+            return GPIO_PORTB_BASE;
+        case 3:
+            return GPIO_PORTC_BASE;
+        case 4: 
+            return GPIO_PORTD_BASE;
+        case 5:
+            return GPIO_PORTE_BASE;
+        case 6: 
+            return GPIO_PORTF_BASE;
+        default:
+            return 0x00000000;
+    }
+}
+
+
---
+++ map.h
@@ -0,0 +1,7 @@
+#ifndef __MAP_H__
+#define __MAP_H__
+
+uint32_t MapPin(ioPin_t ioPin);
+uint32_t MapPort(ioPin_t ioPin);
+
+#endif
